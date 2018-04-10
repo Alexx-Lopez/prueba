5fd3c58bd30538aca8fdfa40d1461fec87ca839b
Alexx <eralexxnoyi@gmail.com>
2018-04-10 14:07:26 -0600

    ultima actualización

--- Proyecto-prueba/Proyecto-prueba/Form1.cs
+++ Proyecto-prueba/Proyecto-prueba/Form1.cs
@@ -22,7 +22,9 @@ namespace Proyecto_prueba
             //Comentario 1
             //Comentario 2
             //Comentario 3
-            //Comentario 4
+            //Comentario 4 
+
+
         }
     }
 }
