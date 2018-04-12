dac231ef410942089256812958a625db500b5dfc
ErickNoyola <38258825+ErickNoyola@users.noreply.github.com>
2018-04-12 17:28:10 -0500

    añadi el boton 3

--- Proyecto-prueba/Proyecto-prueba/Form1.Designer.cs
+++ Proyecto-prueba/Proyecto-prueba/Form1.Designer.cs
@@ -29,6 +29,7 @@
         private void InitializeComponent()
         {
             this.button1 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button1
@@ -40,11 +41,22 @@
             this.button1.Text = "button1";
             this.button1.UseVisualStyleBackColor = true;
             // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(286, 37);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 1;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.ClientSize = new System.Drawing.Size(625, 261);
+            this.Controls.Add(this.button3);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Form1";
@@ -56,6 +68,7 @@
         #endregion
 
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button3;
     }
 }
 
--- Proyecto-prueba/Proyecto-prueba/Form1.cs
+++ Proyecto-prueba/Proyecto-prueba/Form1.cs
@@ -21,5 +21,10 @@ namespace Proyecto_prueba
         {
            
         }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Mauri quiere");
+        }
     }
 }
